f9cd215388c48b5f304b27a2f9fb9d94854e224a
Shakir Ullah <sullah@ariasystems.com>
2025-10-26 18:08:45 +0500

    refactor: update project configuration and remove tsup

--- vite.config.ts
+++ vite.config.ts
@@ -1,52 +1,30 @@
-/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-import { playwright } from '@vitest/browser-playwright';
-const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+import dts from "vite-plugin-dts";
 
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    lib: {
-      entry: "src/index.ts",
-      name: "ReactHeadlessUIKit",
-      fileName: format => `react-headless-ui-kit.${format}.js`
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM"
-        }
-      }
-    }
-  },
-  test: {
-    projects: [{
-      extends: true,
-      plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
-      test: {
-        name: 'storybook',
-        browser: {
-          enabled: true,
-          headless: true,
-          provider: playwright({}),
-          instances: [{
-            browser: 'chromium'
-          }]
+    base: "",
+    plugins: [
+        react(),
+        dts({
+            insertTypesEntry: true, // generates index.d.ts
+            outDir: "dist", // ensures types go to dist/
+        }),
+    ],
+    build: {
+        lib: {
+            entry: "src/index.ts",
+            name: "ReactHeadlessUIKit",
+            fileName: (format) => `react-headless-ui-kit.${format}.js`,
         },
-        setupFiles: ['.storybook/vitest.setup.ts']
-      }
-    }]
-  }
+        rollupOptions: {
+            external: ["react", "react-dom"],
+            output: {
+                globals: {
+                    react: "React",
+                    "react-dom": "ReactDOM",
+                },
+            },
+        },
+    },
 });
